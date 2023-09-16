093c5438e4371df65c610259c30604ded971b13a
gregorysharkov <33329613+gregorysharkov@users.noreply.github.com>
2023-09-16 16:00:44 +0000

    refactor: cleaning up the repo

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
 
-from flask import Flask
-app = Flask(__name__)
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+import tore.spiders as spiders
+
+
+class App:
+    def __init__(self):
+        self.runner = CrawlerRunner()
+        settings = get_project_settings()
+        configure_logging(settings)
+
+    @defer.inlineCallbacks
+    def run(self):
+        # Add all of the spiders that you want to launch to the runner.
+        yield self.runner.crawl(spiders.EpisodeScraper)
+        yield self.runner.crawl(spiders.DocumentaryScraper)
+        yield self.runner.crawl(spiders.StereoScraper)
+        yield self.runner.crawl(spiders.EpisodeScraper)
+        reactor.stop()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
+    reactor.run()
